561802cc2703668c959ede9a9ba1d4107385f15e
Ankit <ankit.tiwari12192@gmail.com>
2017-10-16 11:06:02 +0530

    Beta release

--- testfile.py
+++ testfile.py
@@ -1,43 +1,78 @@
-import requests
-import json
-
-# res= requests.get("http://0.0.0.0:5010/listusers/3")
-
-#print res.content
-
-
-# json1 = {"name ":"ankit",
-#         "id": "10"}
+# import requests
+# import json
 #
-# data = json.dumps(json1)
-# print data
+# # res= requests.get("http://0.0.0.0:5010/listusers/3")
 #
-# ress = requests.post("http://0.0.0.0:5210/adduser",data=data)
+# #print res.content
 #
-# print ress.content
-
-import urllib2
 #
-# pr = {
-#     "name": "morpheus",
-#     "job": "zion resident",
-#     "id":"10"
+# # json1 = {"name ":"ankit",
+# #         "id": "10"}
+# #
+# # data = json.dumps(json1)
+# # print data
+# #
+# # ress = requests.post("http://0.0.0.0:5210/adduser",data=data)
+# #
+# # print ress.content
+#
+# import urllib2
+# #
+# # pr = {
+# #     "name": "morpheus",
+# #     "job": "zion resident",
+# #     "id":"10"
+# # }
+#
+# # res2 = requests.delete("http://0.0.0.0:5010/deleteuser/4")
+# # print res2.content
+# # print res2.status_code
+# # payload_data = json.dumps(json1)
+# #
+# # resm= requests.post(url='http://0.0.0.0:5010/adduser',data=payload_data)
+# # print resm.content
+# # print res.status_code
+#
+#
+# data = {"id":"3",
+#         "Name":"Shu"
 # }
-
-# res2 = requests.delete("http://0.0.0.0:5010/deleteuser/4")
-# print res2.content
-# print res2.status_code
-# payload_data = json.dumps(json1)
 #
-# resm= requests.post(url='http://0.0.0.0:5010/adduser',data=payload_data)
-# print resm.content
-# print res.status_code
+# res3 = requests.put("https://reqres.in/api/users/2",data=data)
+#
+# print res3.content
 
 
-data = {"id":"3",
-        "Name":"Shu"
-}
 
-res3 = requests.put("https://reqres.in/api/users/2",data=data)
 
-print res3.content
+
+
+
+
+
+
+
+
+
+
+url = "http://naveenaws.mynewtonhq.com"
+
+url_stage = "https://staging.mynewtonhq.com"
+
+
+import webbrowser
+import requests
+import requests.exceptions
+
+
+try:
+        status = requests.get(url)
+        print status.status_code
+
+
+except:
+        print "Server might not be running "
+        webbrowser.open(url_stage)
+
+
+
