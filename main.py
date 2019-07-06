20c6f23153ab11b6488ba1eaab5a0e0b65d3595b
Ankit Tiwari <ankit.tiwari12192@gmail.com>
2019-07-06 12:57:05 +0530

    Added host to fix

--- main.py
+++ main.py
@@ -104,4 +104,4 @@ def render_500(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run( port=port, debug=True)
+    app.run(host='0.0.0.0',port=port, debug=False)
